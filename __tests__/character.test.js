89588901d2e6dabf9d0870b52053e8228e33376d
PaladinKnightMaster <paladinknightmaster@gmail.com>
2023-05-03 04:50:25 -0700

    fix jest

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { LordOfRingsAPI } from '../src/services/LordOfRingsAPI';
 
-
 describe('character API', () => {
   test('get all characters', async () => {
     const response = await LordOfRingsAPI.get('/character');
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.js'],
   // Other Jest configurations if needed
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
-  }
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
 };
